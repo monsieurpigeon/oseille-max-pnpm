abb6b398e257b915fe4491d5b29b63a32990247c
Maxime PIGEON <maxpige@gmail.com>
2024-10-25 22:06:56 +0200

    add variables

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -16,7 +16,9 @@ function App() {
   return (
     <div>
       <div>{import.meta.env.VITE_TEST_VAR}</div>
-      <div>{import.meta.env.PROD ? "PRODUCTION" : "PAS PRODUCTION"}</div>
+      <div>MODE : {import.meta.env.MODE}</div>
+      <div>SSR : {import.meta.env.SSR ? "SSR" : "PAS SSR"}</div>
+      <div>PROD : {import.meta.env.PROD ? "PRODUCTION" : "PAS PRODUCTION"}</div>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
