58ff00081a9ecc9162cca0d056ff432eedd28fa0
Maxime PIGEON <maxpige@gmail.com>
2024-10-25 21:33:25 +0200

    first commit

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+    proxy: {
+      "/api": {
+        target:
+          process.env.NODE_ENV === "docker"
+            ? "http://server_c:3333"
+            : "http://localhost:3333",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
